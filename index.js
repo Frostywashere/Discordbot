c9e00b987b98d9a2cd5df2cec3b420f7995a5973
Frostywashere <johnlukeadams13@gmail.com>
2026-09-07 18:33:48 -0500

    Fix title formatting in createTicketPanel function

--- index.js
+++ index.js
@@ -598,7 +598,7 @@ function createTicketPanel() {
   const embed = new EmbedBuilder()
     .setColor(0x0066ff)
     .setTitle(
-      "🎟️ Atlanta Heights Support Tickets"
+      " Atlanta Heights Support Tickets"
     )
     .setDescription(
       "Welcome to Atlanta Heights Support. To ensure your issue is handled as quickly as possible, please select the most relevant category below.\n\n" +
